d512f1bfb0a05abb10069ea64ea65dc06cfe6ebf
Mikhail Grigoriev <mgrigoriev@gmail.com>
2024-05-11 01:06:09 +0300

    Add validation

--- chatmessages/go.mod
+++ chatmessages/go.mod
@@ -3,16 +3,21 @@ module github.com/mgrigoriev/chat-monorepo/chatmesages
 go 1.22.0
 
 require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.34.1-20240508200655-46a4cf4ba109.1
+	github.com/bufbuild/protovalidate-go v0.6.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1
-	google.golang.org/genproto/googleapis/api v0.0.0-20240227224415-6ceb2ff114de
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de
+	google.golang.org/genproto/googleapis/api v0.0.0-20240401170217-c3f982113cda
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda
 	google.golang.org/grpc v1.63.2
 	google.golang.org/protobuf v1.34.1
 )
 
 require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/google/cel-go v0.20.1 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto v0.0.0-20240227224415-6ceb2ff114de // indirect
 )
--- chatmessages/internal/server/server.go
+++ chatmessages/internal/server/server.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	"context"
+	"errors"
+	"fmt"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -11,6 +14,7 @@ import (
 	"sync"
 	"sync/atomic"
 
+	"github.com/bufbuild/protovalidate-go"
 	pb "github.com/mgrigoriev/chat-monorepo/chatmesages/pkg/api/chatmessages"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
@@ -26,13 +30,29 @@ type server struct {
 	mx                  sync.RWMutex
 	serverChatMessages  map[uint64]*pb.ChatMessageInfo
 	privateChatMessages map[uint64]*pb.ChatMessageInfo
+	validator           *protovalidate.Validator
 }
 
-func NewServer() *server {
-	return &server{
+func NewServer() (*server, error) {
+	srv := &server{
 		serverChatMessages:  make(map[uint64]*pb.ChatMessageInfo),
 		privateChatMessages: make(map[uint64]*pb.ChatMessageInfo),
 	}
+
+	validator, err := protovalidate.New(
+		protovalidate.WithDisableLazy(true),
+		protovalidate.WithMessages(
+			&pb.SaveChatMessageRequest{},
+			&pb.ListPrivateChatMessagesRequest{},
+			&pb.ListServerChatMessagesRequest{},
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize validator: %w", err)
+	}
+
+	srv.validator = validator
+	return srv, nil
 }
 
 func Start() {
@@ -40,11 +60,10 @@ func Start() {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	server := NewServer()
-	//server, err := NewServer()
-	//if err != nil {
-	//	log.Fatalf("failed to create server: %v", err)
-	//}
+	server, err := NewServer()
+	if err != nil {
+		log.Fatalf("failed to create server: %v", err)
+	}
 
 	var wg sync.WaitGroup
 
@@ -98,28 +117,62 @@ func Start() {
 		if err := httpServer.Serve(lis); err != nil {
 			log.Fatalf("failed to serve: %v", err)
 		}
+
+		// SaveChatMessage:
+		// curl --location 'localhost:8080/api/v1/chatmessages' --header 'Content-Type: application/json' --data '{"user_id":1, "user_name":"john", "recipient_type":1, "recipient_id":10, "content":"hello - private message"}'
+		// curl --location 'localhost:8080/api/v1/chatmessages' --header 'Content-Type: application/json' --data '{"user_id":1, "user_name":"john", "recipient_type":2, "recipient_id":1, "content":"hello - server message"}'
+		// ListServerChatMessages:
+		// curl --location 'localhost:8080/api/v1/chatmessages/server?server_id=1'
+		// ListPrivateChatMessages:
+		// curl --location 'localhost:8080/api/v1/chatmessages/private?user_id=1&other_user_id=10'
 	}()
 
 	wg.Wait()
+}
 
-	// SaveChatMessage:
-	// curl --location 'localhost:8080/api/v1/chatmessages' --header 'Content-Type: application/json' --data '{"user_id":1, "user_name":"john", "recipient_type":1, "recipient_id":10, "content":"hello - private message"}'
-	// curl --location 'localhost:8080/api/v1/chatmessages' --header 'Content-Type: application/json' --data '{"user_id":1, "user_name":"john", "recipient_type":2, "recipient_id":1, "content":"hello - server message"}'
-	// ListServerChatMessages:
-	// curl --location 'localhost:8080/api/v1/chatmessages/server?server_id=1'
-	// ListPrivateChatMessages:
-	// curl --location 'localhost:8080/api/v1/chatmessages/private?user_id=1&other_user_id=10'
+func protovalidateViolationsToGoogleViolations(vs []*validate.Violation) []*errdetails.BadRequest_FieldViolation {
+	res := make([]*errdetails.BadRequest_FieldViolation, len(vs))
+	for i, v := range vs {
+		res[i] = &errdetails.BadRequest_FieldViolation{
+			Field:       v.FieldPath,
+			Description: v.Message,
+		}
+	}
+	return res
+}
+
+func convertProtovalidateValidationErrorToErrdetailsBadRequest(valErr *protovalidate.ValidationError) *errdetails.BadRequest {
+	return &errdetails.BadRequest{
+		FieldViolations: protovalidateViolationsToGoogleViolations(valErr.Violations),
+	}
+}
+
+func rpcValidationError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	var valErr *protovalidate.ValidationError
+	if ok := errors.As(err, &valErr); ok {
+		st, err := status.New(codes.InvalidArgument, codes.InvalidArgument.String()).
+			WithDetails(convertProtovalidateValidationErrorToErrdetailsBadRequest(valErr))
+		if err == nil {
+			return st.Err()
+		}
+	}
+
+	return status.Error(codes.Internal, err.Error())
 }
 
 // SaveChatMessage implements pb.ChatMessagesServiceServer
 func (s *server) SaveChatMessage(_ context.Context, req *pb.SaveChatMessageRequest) (*pb.SaveChatMessageResponse, error) {
+	if err := s.validator.Validate(req); err != nil {
+		return nil, rpcValidationError(err)
+	}
+
 	info := req.GetInfo()
 
 	log.Printf("SaveChatMessage: received: %s", info.GetContent())
-
-	if err := validateSaveChatMessageRequest(req); err != nil {
-		return nil, err
-	}
 
 	id := atomic.AddUint64(&idSerial, 1)
 
@@ -136,63 +189,12 @@ func (s *server) SaveChatMessage(_ context.Context, req *pb.SaveChatMessageReque
 	}, nil
 }
 
-func validateSaveChatMessageRequest(req *pb.SaveChatMessageRequest) error {
-	info := req.GetInfo()
-
-	var violations []*errdetails.BadRequest_FieldViolation
-
-	if len(info.GetContent()) == 0 {
-		violations = append(violations, &errdetails.BadRequest_FieldViolation{
-			Field:       "info.content",
-			Description: "empty",
-		})
-	}
-
-	if info.GetUserId() == 0 {
-		violations = append(violations, &errdetails.BadRequest_FieldViolation{
-			Field:       "info.user_id",
-			Description: "empty",
-		})
-	}
-
-	if info.GetRecipientId() == 0 {
-		violations = append(violations, &errdetails.BadRequest_FieldViolation{
-			Field:       "info.recipient_id",
-			Description: "empty",
-		})
-	}
-
-	if len(info.GetUserName()) == 0 {
-		violations = append(violations, &errdetails.BadRequest_FieldViolation{
-			Field:       "info.user_name",
-			Description: "empty",
-		})
-	}
-
-	if info.RecipientType != pb.ChatMessageInfo_USER && info.RecipientType != pb.ChatMessageInfo_SERVER {
-		violations = append(violations, &errdetails.BadRequest_FieldViolation{
-			Field:       "info.recipient_type",
-			Description: "invalid",
-		})
-	}
-
-	if len(violations) > 0 {
-		st, err := status.New(codes.InvalidArgument, codes.InvalidArgument.String()).
-			WithDetails(&errdetails.BadRequest{
-				FieldViolations: violations,
-			})
-		if err != nil {
-			return status.Error(codes.Internal, err.Error())
-		}
-
-		return st.Err()
-	}
-
-	return nil
-}
-
 // ListServerChatMessages implements pb.ChatMessagesServiceServer
 func (s *server) ListServerChatMessages(_ context.Context, req *pb.ListServerChatMessagesRequest) (*pb.ListChatMessagesResponse, error) {
+	if err := s.validator.Validate(req); err != nil {
+		return nil, rpcValidationError(err)
+	}
+
 	serverID := req.GetServerId()
 
 	log.Println("ListServerChatMessages: received")
@@ -216,6 +218,10 @@ func (s *server) ListServerChatMessages(_ context.Context, req *pb.ListServerCha
 }
 
 func (s *server) ListPrivateChatMessages(_ context.Context, req *pb.ListPrivateChatMessagesRequest) (*pb.ListChatMessagesResponse, error) {
+	if err := s.validator.Validate(req); err != nil {
+		return nil, rpcValidationError(err)
+	}
+
 	userID := req.UserId
 	otherUserID := req.OtherUserId
 
